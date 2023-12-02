43816f8b7a083db969d82e9c7508e604b9b6bdd2
Maylida289 <4519210082@univpancasila.ac.id>
2023-12-02 22:04:22 +0700

    add repository

---
+++ repository/content/factory.go
@@ -0,0 +1,13 @@
+package content
+
+import (
+	"hexa/util"
+
+	"hexa/business/content"
+	// "google.golang.org/api/content/v2"
+)
+
+func RepositoryFactory(dbCon *util.DatabaseConnection) (contentRepo content.Repository) {
+
+	return
+}
