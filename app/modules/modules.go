1bb66650a4e93dc101805023cdc7b02798e7b913
Maylida289 <4519210082@univpancasila.ac.id>
2023-12-07 21:17:50 +0700

    add dependency injection

---
+++ app/modules/modules.go
@@ -0,0 +1,24 @@
+//untuk registrasi modul atau implementasi dependendy injection nya
+package modules
+
+import (
+	"hexa/api"
+	"hexa/util"
+
+	// "github.com/swaggo/swag/example/basic/api"
+	contentController "hexa/api/v1/content"
+	contentService "hexa/business/content"
+	contentRepo "hexa/repository/content"
+)
+
+func RegisterModules(dbCon *util.DatabaseConnection) api.Controller {
+	contentPermitRepository := contentRepo.RepositoryFactory(dbCon)
+	contentPermitService := contentService.NewService(contentPermitRepository)
+	contentPermitController := contentController.NewController(contentPermitService)
+
+	controllers := api.Controller{
+		ContentController: contentPermitController,
+	}
+
+	return controllers
+}
